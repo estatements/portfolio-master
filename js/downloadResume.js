83ff5b05bd1c1043e9edf21c82bb89dc93ec088a
Daniel <statements@dek-data.com>
2023-09-12 00:13:42 -0500

    edits

--- js/downloadResume.js
+++ js/downloadResume.js
@@ -2,12 +2,12 @@ document
   .getElementById("downloadButton")
   .addEventListener("click", function () {
     // Define the path to your PDF file in the "assets" folder
-    const pdfFilePath = "https://estatements.github.io/portfolio-master/assets/CV_charles.pdf";
+    const pdfFilePath = "/assets/CV_charles.pdf";
 
     // Create a hidden anchor element to trigger the download
     const downloadLink = document.createElement("a");
     downloadLink.href = pdfFilePath;
-    downloadLink.download = "https://estatements.github.io/portfolio-master/assets/CV_charles.pdf";
+    downloadLink.download = "/assets/CV_charles.pdf";
 
     // Trigger the click event of the anchor element to start the download
     downloadLink.click();
